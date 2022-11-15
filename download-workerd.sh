0d4eb35e822e3dace6b3fb08bee9c94db6ba0e27
Tommy Chen <tommy351@gmail.com>
2022-11-15 16:09:06 +0800

    fix: Fix ARM image

--- download-workerd.sh
+++ download-workerd.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-pkg_version=1.20220926.2
+pkg_version=1.20221111.4
 
-if [ $ARCH == arm* ]; then
+if [[ $ARCH == arm* ]]; then
   pkg_arch=arm64
 else
   pkg_arch=64
 fi
 
+set -x
 curl -L https://registry.npmjs.org/@cloudflare/workerd-linux-${pkg_arch}/-/workerd-linux-${pkg_arch}-${pkg_version}.tgz | tar -xz --strip-components 1
